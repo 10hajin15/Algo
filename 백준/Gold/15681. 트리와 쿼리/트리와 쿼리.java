cb908347fc82e671886edc6df4e8e52e0aad57ff
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2024-11-12 10:56:09 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1200 ms, Memory: 90276 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,7 +12,6 @@ public class Main {
 
     static int dfs(int root) {
         visited[root] = true;
-        tree[root] = 1;
 
         for(int i: graph[root]) {
             if(visited[i]) continue;
@@ -29,6 +29,7 @@ public class Main {
         int Q = Integer.parseInt(st.nextToken());
 
         tree = new int[N+1];
+        Arrays.fill(tree, 1);
         graph = new ArrayList[N+1];
         for(int i=0; i<N+1; i++) {
             graph[i] = new ArrayList<>();
