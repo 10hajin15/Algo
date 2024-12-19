ab5633a7fd45b14c8bc2f71c5b65714cc6c50081
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2024-12-19 20:54:16 +0900

    [Silver IV] Title: 문자열 집합, Time: 352 ms, Memory: 30996 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,9 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static String[] sLst;
+
+    static boolean search(String str) {
+        int left = 0;
+        int right = sLst.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (sLst[mid].equals(str)) {
+                return true;
+            }
+
+            if (sLst[mid].compareTo(str) < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,67 +35,19 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int ans = 0;
-
-        Trie trie = new Trie();
-
+        sLst = new String[N];
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            trie.insert(str);
+            sLst[i] = br.readLine();
         }
+        Arrays.sort(sLst);
 
+        int ans = 0;
         for (int i = 0; i < M; i++) {
-            String str = br.readLine();
-            if(trie.search(str)) ans++;
+            if (search(br.readLine())) {
+                ans++;
+            }
         }
 
         System.out.println(ans);
-    }
-
-    static class Node {
-        Node[] child = new Node[26];
-        boolean endOfWord;
-
-        public Node() {}
-    }
-
-    static class Trie {
-        Node root;
-
-        public Trie() {
-            this.root = new Node();
-        }
-
-        void insert(String str) {
-            Node node = this.root;
-
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-
-                if(node.child[c-'a'] == null) {
-                    node.child[c-'a'] = new Node();
-                }
-
-                node = node.child[c-'a'];
-            }
-
-            node.endOfWord = true;
-        }
-
-        boolean search(String str) {
-            Node node = this.root;
-
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-
-                if(node.child[c-'a'] == null) {
-                    return false;
-                } else {
-                    node = node.child[c-'a'];
-                }
-            }
-
-            return node.endOfWord;
-        }
     }
 }
