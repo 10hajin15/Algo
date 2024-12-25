c8ecea66ddb192ce76dcd6cd430c7f16125a9a77
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2024-12-25 21:55:06 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 564 ms, Memory: 53372 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -1,46 +1,87 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] tree;
+
+    static void init(int node, int start, int end) {
+        if (start == end) {
+            tree[node] = start;
+        } else {
+            int mid = (start + end) / 2;
+            init(node * 2, start, mid);
+            init(node * 2 + 1, mid + 1, end);
+            tree[node] = compare(tree[node * 2], tree[node * 2 + 1]);
+        }
+    }
+
+    static int compare(int x, int y) {
+        if (arr[x] < arr[y]) return x;
+        else if (arr[x] > arr[y]) return y;
+        else return Math.min(x, y);
+    }
+
+    static void update(int node, int start, int end, int idx, int value) {
+        if (start == end) {
+            arr[idx] = value;
+        } else {
+            int mid = (start + end) / 2;
+            if (idx <= mid) update(node * 2, start, mid, idx, value);
+            else update(node * 2 + 1, mid + 1, end, idx, value);
+            tree[node] = compare(tree[node * 2], tree[node * 2 + 1]);
+        }
+    }
+
+    static int query(int node, int start, int end, int left, int right) {
+        if (left > end || right < start) return -1;
+        if (left <= start && end <= right) return tree[node];
+
+        int mid = (start + end) / 2;
+
+        int lQuery = query(node * 2, start, mid, left, right);
+        int rQuery = query(node * 2 + 1, mid + 1, end, left, right);
+
+        if (lQuery == -1) return rQuery;
+        if (rQuery == -1) return lQuery;
+
+        return compare(lQuery, rQuery);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
-
-        for (int i = 1; i <= n; i++) {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            map.computeIfAbsent(arr[i], k -> new TreeSet<>()).add(i);
         }
 
-        int m = Integer.parseInt(br.readLine());
+        int k = 0;
+        while((1<<k) <= N) k++;
+        tree = new int[(1 << (k + 1))];
 
-        for (int q = 0; q < m; q++) {
+        init(1, 0, N - 1);
+
+        M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int queryType = Integer.parseInt(st.nextToken());
-
-            if (queryType == 1) {
-                int i = Integer.parseInt(st.nextToken());
-                int v = Integer.parseInt(st.nextToken());
-
-                int oldVal = arr[i];
-                map.get(oldVal).remove(i);
-                if (map.get(oldVal).isEmpty()) {
-                    map.remove(oldVal);
-                }
-
-                arr[i] = v;
-                map.computeIfAbsent(v, k -> new TreeSet<>()).add(i);
-            } else if (queryType == 2) {
-                int minValue = map.firstKey();
-                int minIndex = map.get(minValue).first();
-                sb.append(minIndex).append("\n");
+            int type = Integer.parseInt(st.nextToken());
+            if (type == 1) {
+                int idx = Integer.parseInt(st.nextToken()) - 1;
+                int value = Integer.parseInt(st.nextToken());
+                update(1, 0, N - 1, idx, value);
+            } else if (type == 2) {
+                int resultIdx = query(1, 0, N - 1, 0, N - 1);
+                sb.append(resultIdx + 1).append("\n");
             }
         }
-
         System.out.print(sb);
     }
 }
