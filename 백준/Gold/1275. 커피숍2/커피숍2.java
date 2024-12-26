fca7f41538d1cb2a65399b8f3c4cdf2cabfd9921
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2024-12-26 19:45:10 +0900

    [Gold I] Title: 커피숍2, Time: 784 ms, Memory: 92492 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long[] arr;
+    static long[] tree;
+
+    static void init(int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            init(node * 2, start, mid);
+            init(node * 2 + 1, mid + 1, end);
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+    }
+
+    static void update(int node, int start, int end, int idx, long val) {
+        if (idx < start || idx > end) {
+            return;
+        }
+        if (start == end) {
+            arr[idx] = val;
+            tree[node] = val;
+            return;
+        }
+        int mid = (start + end) / 2;
+        update(node * 2, start, mid, idx, val);
+        update(node * 2 + 1, mid + 1, end, idx, val);
+        tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    }
+
+    static long query(int node, int start, int end, int left, int right) {
+        if (left > end || right < start) {
+            return 0;
+        }
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        long lsum = query(node * 2, start, mid, left, right);
+        long rsum = query(node * 2 + 1, mid + 1, end, left, right);
+        return lsum + rsum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        arr = new long[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        int h = 0;
+        while((1<<h) <= N) h++;
+        tree = new long[(1 << (h + 1))];
+
+        init(1, 0, N - 1);
+
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            long b = Long.parseLong(st.nextToken());
+
+            sb.append(query(1, 0, N - 1, Math.min(x, y), Math.max(x, y))).append('\n');
+            update(1, 0, N - 1, a, b);
+        }
+
+        System.out.print(sb);
+    }
+}
