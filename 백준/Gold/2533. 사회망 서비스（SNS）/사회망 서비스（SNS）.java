31497563776e059a0dca4db2f3f192d4b370e9a2
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2025-01-23 11:35:51 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1992 ms, Memory: 427580 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static List<Integer>[] tree;
+    static int[][] dp;
+    static boolean[] visited;
+
+    static void dfs(int start) {
+        visited[start] = true;
+
+        for(int nxt: tree[start]) {
+            if(!visited[nxt]) {
+                dfs(nxt);
+
+                dp[start][0] += dp[nxt][1];
+                dp[start][1] += Math.min(dp[nxt][0], dp[nxt][1]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        tree = new ArrayList[N + 1];
+        dp = new int[N+1][2];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i < N+1; i++) {
+            tree[i] = new ArrayList<>();
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 0; i < N-1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            tree[u].add(v);
+            tree[v].add(u);
+        }
+
+        dfs(1);
+
+        System.out.println(Math.min(dp[1][0], dp[1][1]));
+    }
+}
