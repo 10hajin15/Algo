efb0aec878d45675caa4c52f57bbc773daed3700
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2025-03-18 14:19:47 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 46.05 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -0,0 +1,60 @@
+class Solution {
+    static int N, M;
+    static int count, sum;
+    static int[] emoti;
+    static int[][] uLst;
+
+    void calc(int[] lst) {
+        int tCount = 0;
+        int tSum = 0;
+
+        int[] sLst = new int[N];
+
+        for (int i = 0; i < M; i++) {
+            int price = emoti[i] - (emoti[i] * lst[i] / 100);
+            for (int j = 0; j < N; j++) {
+                int[] u = uLst[j];
+                if(u[0] > lst[i]) continue;
+                sLst[j] += price;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(sLst[i] >= uLst[i][1]) tCount++;
+            else tSum += sLst[i];
+        }
+
+        if(tCount >= count) {
+            if(tCount == count && tSum < sum) return;
+            count = tCount;
+            sum = tSum;
+        }
+    }
+
+    void dfs(int depth, int[] lst) {
+        if(depth == M) {
+            calc(lst);
+            return;
+        }
+
+        for (int i = 10; i <= 40; i+=10) {
+            lst[depth] = i;
+            dfs(depth+1, lst);
+        }
+    }
+    
+    public int[] solution(int[][] users, int[] emoticons) {
+        count = Integer.MIN_VALUE;
+        sum = Integer.MIN_VALUE;
+
+        N = users.length;
+        M = emoticons.length;
+
+        emoti = emoticons;
+        uLst = users;
+
+        dfs(0, new int[M]);
+
+        return new int[] {count, sum};
+    }
+}
