316e3bacc73f8bdd1e3685bfe26a6fbf115e1334
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2025-06-23 15:18:12 +0900

    [Gold III] Title: 오등큰수, Time: 976 ms, Memory: 144608 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -2,62 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    static int[] answer;
-    static Map<Integer, Integer> counts;
-    static TreeSet<Integer> indexSet = new TreeSet<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        arr = new int[N];
-        answer = new int[N];
-        counts = new HashMap<>();
+        int[] arr = new int[N];
+        int[] answer = new int[N];
+        int[] freq = new int[1000001];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            counts.put(arr[i], counts.getOrDefault(arr[i], 0) + 1);
+            freq[arr[i]]++;
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (a[1] == b[1]) return a[0] - b[0];
-            return b[1] - a[1];
-        });
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < N; i++) {
-            pq.add(new int[]{i, counts.get(arr[i])});
-        }
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curIdx = cur[0];
-            int curCnt = cur[1];
-
-            List<Integer> sameGroup = new ArrayList<>();
-            sameGroup.add(curIdx);
-            while (!pq.isEmpty() && pq.peek()[1] == curCnt) {
-                sameGroup.add(pq.poll()[0]);
+        for (int i = N - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && freq[arr[i]] >= freq[arr[stack.peek()]]) {
+                stack.pop();
             }
 
-            for (int idx : sameGroup) {
-                Integer nextIdx = indexSet.higher(idx);
-                if (nextIdx == null) {
-                    answer[idx] = -1;
-                } else {
-                    answer[idx] = arr[nextIdx];
-                }
+            if (stack.isEmpty()) {
+                answer[i] = -1;
+            } else {
+                answer[i] = arr[stack.peek()];
             }
 
-            indexSet.addAll(sameGroup);
+            stack.push(i);
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int a : answer) {
-            sb.append(a).append(" ");
+        for (int x : answer) {
+            sb.append(x).append(" ");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
