c63a117ea5e1363731aa5ce0a5f8937260333fd8
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2025-06-26 11:32:05 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 488 ms, Memory: 96260 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        int[] LIS = new int[N];
+        LIS[0] = arr[0];
+        int count = 1;
+        for (int i = 1; i < N; i++) {
+            int num = arr[i];
+
+            if(LIS[count-1] < num) {
+                LIS[count] = num;
+                count++;
+            } else {
+                int left = 0;
+                int right = count;
+
+                while(left < right) {
+                    int mid = (left + right) / 2;
+
+                    if(LIS[mid] < num) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+
+                LIS[left] = num;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
