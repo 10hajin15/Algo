ce863657f24a85596d25e7c1c17b05a74f1f3577
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2025-07-03 22:55:21 +0900

    [Gold II] Title: 후위 표기식, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input) {
+            if ('A' <= c && c <= 'Z') {
+                sb.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && calc(stack.peek()) >= calc(c)) {
+                    if (stack.peek() == '(') break;
+                    sb.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static int calc(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0;
+    }
+}
