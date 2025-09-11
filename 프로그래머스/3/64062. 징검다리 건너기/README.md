6eaa126e340150d3bbec38e30f3fe740ab9fdd3b
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2025-09-11 20:59:55 +0900

    [level 3] Title: 징검다리 건너기, Time: 27.88 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 64.1 MB, 시간: 46.31 ms
+메모리: 64 MB, 시간: 27.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 11일 19:06:55
+2025년 09월 11일 20:59:53
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,52 +1,49 @@
 class Solution {
     static int answer;
-    
+
     boolean isValid(int[] stones, int k, int mid) {
         int cnt = 0;
 
-            for(int i=0; i<stones.length; i++) {
-                if(cnt == k) {
-                    return false;
-                }
-                if(stones[i] >= mid) {
-                    cnt = 0;
-                } else {
-                    cnt++;
-                }
+        for(int i=0; i<stones.length; i++) {
+            if(cnt == k) {
+                return false;
             }
+            if(stones[i] >= mid) {
+                cnt = 0;
+            } else {
+                cnt++;
+            }
+        }
 
-            if(cnt == k) return false;
+        if(cnt == k) return false;
 
-            answer = Math.max(answer, mid);
-            return true;
+        answer = Math.max(answer, mid);
+        return true;
     }
-    
+
     public int solution(int[] stones, int k) {
         answer = 0;
-        
-        
+        int left = 1;
+        int right = Integer.MIN_VALUE;
 
-            int left = 1;
-            int right = Integer.MIN_VALUE;
+        for(int i=0; i<stones.length; i++) {
+            right = Math.max(right, stones[i]);
+        }
 
-            for(int i=0; i<stones.length; i++) {
-                right = Math.max(right, stones[i]);
-            }
-        
         if(stones.length == 1) return right;
-        
+
         if(right == k) return k;
 
-            while(left < right) {
-                int mid = (left + right) / 2;
+        while(left < right) {
+            int mid = (left + right) / 2;
 
-                if(isValid(stones, k, mid)) {
-                    left = mid+1;
-                } else {
-                    right = mid;
-                }
+            if(isValid(stones, k, mid)) {
+                left = mid+1;
+            } else {
+                right = mid;
             }
+        }
 
-            return answer;
+        return answer;
     }
 }
